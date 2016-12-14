d766105a8d0dc7de0ac057cc2629dd8f34879cbd
Ian VanSchooten <ian.vanschooten@gmail.com>
2016-12-14 17:08:15 -0500

    Add a button in an overlay hole in the demo
    This adds a few things to the example:
    
    1. Adds a button next to “Mission”
    
    2. Makes that button advance the joyride
    
    3. Removes the “Close” button on the Mission joyride, so the button
    must be clicked.
    
    4. Stores the step index in local state, to disable the overlay on
    the “Mission” slide, forcing the button to be clicked to advance.

--- test/demo/App.jsx
+++ test/demo/App.jsx
@@ -129,6 +129,21 @@ export default class Demo extends React.Component {
     }
   }
 
+  handleNextButtonClick() {
+    if (this.state.step === 1) {
+      this.setState({
+        running: false,
+        step: 2,
+      });
+      // allow the tooltip time to hide before restarting
+      setTimeout(() => {
+        this.setState({
+          running: true,
+        });
+      }, 500);
+    }
+  }
+
   render() {
     return (
       <div className="demo">
